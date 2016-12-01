e4c944773df6d95d8382ab0899340f3cd4bb7034
vagrant <vagrant@localhost.localdomain>
2016-12-01 21:00:52 +0000

    Some changes in Theory.js

--- app/components/Theory.js
+++ app/components/Theory.js
@@ -6,11 +6,21 @@ class Theory extends React.Component {
 		return (
 		<div>
 			<Navbar signedIn={true}/>
-			<div className="col-md-offset-2 col-md-8 placeholder">
-				Theory view
-			</div>	
-	 	</div>
-	);
+			<div className="col-md-offset-2 col-md-8 placeholder"> 
+				<div> 
+					<p> previous </p>
+					<p>Chapter 1</p>
+					<p> next </p>
+				</div>
+				<div>
+					Text for the theory 
+				</div>
+				<div> 
+					Quiz for the theory
+				</div>
+			</div>
+		</div>)
+
 	}
 }
 
