12cf261bd353aa783f1031e1a2fe427774307171
Jure Malovrh <malovrh.jure@gmail.com>
2017-01-16 22:20:23 +0100

    Update achievements.controller.js

--- server/controllers/achievements.controller.js
+++ server/controllers/achievements.controller.js
@@ -6,13 +6,22 @@ var Achievement = mongoose.model('Achievement');
 
 exports.addNewAchievement = function (req, res) {
 	let achievement = new Achievement(req.body);
-	achievement.save((err, achievement) => {
-		if(err) {
-			res.statusCode = 400;
-			return res.json({error: err});
+	
+	Achievement.findOne({user: achievement.user, text: achievement.text}).exec((err, _achievement) => {
+		if(!err && !_achievement) {
+			achievement.save((err, achievement) => {
+				if(err) {
+					res.statusCode = 400;
+					return res.json({error: err});
+				}
+				res.json({message: achievement});
+			});
+		} else {
+			return res.json({message: _achievement})
 		}
-		res.json({message: achievement});
-	});
+	
+	})
+	
 };
 
 exports.getUserAchievements = function (req, res) {
